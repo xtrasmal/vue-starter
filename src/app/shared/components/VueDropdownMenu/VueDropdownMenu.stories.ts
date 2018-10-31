a8c6b059a7e51082034d9356e13e76c4b731e98e
Johannes Werner <johannes.werner@bcgdv.com>
2018-10-31 13:22:41 +0100

    refactor(storybook): update storybook-addon-vue-info [skip ci]

--- src/app/shared/components/VueDropdownMenu/VueDropdownMenu.stories.ts
+++ src/app/shared/components/VueDropdownMenu/VueDropdownMenu.stories.ts
@@ -1,13 +1,11 @@
 import { storiesOf }   from '@storybook/vue';
 import { action }      from '@storybook/addon-actions';
-import VueInfoAddon    from 'storybook-addon-vue-info';
+import { withInfo }    from 'storybook-addon-vue-info';
 import VueDropdownMenu from './VueDropdownMenu.vue';
 
 const story = (storiesOf('2. Components|DropdownMenu', module) as any);
 
-story.addDecorator(VueInfoAddon);
-
-story.add('Default', () => ({
+story.add('Default', withInfo({})(() => ({
   components: { VueDropdownMenu },
   template:   `<div>This is a dropdown menu within <vue-dropdown-menu @click="action"
 :options="[{ label: 'Save', value: 'save' }, { label: 'Open', value: 'open' }, { label: 'Delete', value: 'delete' }]">
@@ -16,4 +14,4 @@ File
   methods:    {
     action: action('@onClick'),
   },
-}));
+})));
